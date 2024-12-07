d3ce88d848c9fc5097d1c34dabfeb1e8fe6de94f
natalya <natasha-vd@yandex.ru>
2024-12-07 12:27:16 +0300

    add vite-plugin-svgr

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path';
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -15,5 +16,5 @@ export default defineConfig({
       '@hooks': path.resolve(__dirname, './src/hooks'),
     },
   },
-  plugins: [react()],
+  plugins: [svgr(), react()],
 })
